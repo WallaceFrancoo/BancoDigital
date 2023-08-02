9f1c5a1fa2f59953b543ff978c8c90b2ee4ed4a7
Wallace Franco <wallace.franco@outlook.com>
2023-08-02 17:56:14 -0300

    Alterações nos arquivos

--- funcoes.js
+++ funcoes.js
@@ -6,3 +6,13 @@ function MostrarSenha (){
         alterar.type = 'password';
     }
 }
+
+function Acessar(){
+    var nomeAcesso = document.getElementById('nomeLogin').value;
+    if (nomeAcesso.toLowerCase() === 'wallace'){
+        window.alert("Você tem acesso ao sistema")
+    } else {
+        window.alert("Você não tem acesso nesse sistema!!")
+        
+    }
+}
