1c1ca55cccfaa2f72654650ae66186a9782c4ba9
Rodrigo Doratiotto <rodrigo.reis@outlook.com>
2023-08-07 22:48:28 -0300

    Arrumando a estrutura do SQL

--- bancodedados.sql
+++ bancodedados.sql
@@ -7,7 +7,25 @@ CREATE TABLE CADASTRO (
     DataNascimento DATE,
     NumeroCelular CHAR (9),
     Profissao VARCHAR (15),
-    Senha VARCHAR (20),
-    
+    Senha VARCHAR (20)
+ );
 
- )
+ INSERT INTO CADASTRO VALUES(
+    "Rodrigo Doratiotto",
+    "395959731",
+    "rodrigo.reis@outlook.com",
+    "1998-02-14",
+    "958844751",
+    "Dev Jr",
+    "Umasenhaboa"
+ );
+
+  INSERT INTO CADASTRO VALUES(
+    "Walace Francoo",
+    "537417436",
+    "wallace.franco@outlook.com",
+    "2001-02-01",
+    "966374540",
+    "Dev Jr",
+    "Umasenhaboa2"
+ );
