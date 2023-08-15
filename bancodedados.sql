d46cffec4d9e826a4978959ee82417f6222c2e96
Rodrigo Doratiotto <rodrigo.reis@outlook.com>
2023-08-15 19:30:53 -0300

    Alterando nome das informações

--- bancodedados.sql
+++ bancodedados.sql
@@ -5,7 +5,7 @@ CREATE TABLE CADASTRO (
     RG CHAR (9) PRIMARY KEY,
     EmailCliente VARCHAR (80) NOT NULL,
     DataNascimento DATE,
-    NumeroCelular CHAR (9),
+    Contato CHAR (9),
     Profissao VARCHAR (15),
     Senha VARCHAR (20)
  );
